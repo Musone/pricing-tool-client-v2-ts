4e90314201ec1bfa66a19575ed0e3bfbaa89b395
Musone <yakobkh@gmail.com>
2022-07-19 22:33:07 -0700

    Heroku

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,6 +1,7 @@
 const config = {
     // serverUrl: 'http://localhost:5000',
-    serverUrl: 'http://192.168.1.8:5000',
+    // serverUrl: 'http://192.168.1.8:5000',
+    serverUrl: 'https://phare-counselor-finder-api.herokuapp.com',
     localStorageAccessTokenKey: 'phareAccessToken',
     localStorageRefreshTokenKey: 'phareRefreshToken',
 }
