3be97bebb7015e40bcac4db59e70c953a4d1eab8
Musone <yakobkh@gmail.com>
2022-07-23 00:39:17 -0700

    Cleaning technical debt

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,6 +1,4 @@
 const config = {
-    // serverUrl: 'http://localhost:5000',
-    // serverUrl: 'https://phare-counselor-finder-api.herokuapp.com',
     serverUrl: import.meta.env.VITE_SERVER_URL,
     localStorageAccessTokenKey: import.meta.env.VITE_LOCAL_STORAGE_ACCESS_TOKEN_KEY,
     localStorageRefreshTokenKey: import.meta.env.VITE_LOCAL_STORAGE_REFRESH_TOKEN_KEY,
