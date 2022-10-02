f4646367f31487ba6347454c5a1c9fe79eeb6369
Musone <yakobkh@gmail.com>
2022-10-01 17:27:25 -0700

    Heroku

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,10 +1,18 @@
+// const config = {
+//     serverUrl: import.meta.env.VITE_SERVER_URL,
+//     localStorageAccessTokenKey: import.meta.env.VITE_LOCAL_STORAGE_ACCESS_TOKEN_KEY,
+//     localStorageRefreshTokenKey: import.meta.env.VITE_LOCAL_STORAGE_REFRESH_TOKEN_KEY,
+//
+//     defaultCounsellingRate: import.meta.env.VITE_DEFAULT_COUNSELLING_RATE,
+//     defaultSupervisingRate: import.meta.env.VITE_DEFAULT_SUPERVISING_RATE,
+// }
 const config = {
-    serverUrl: import.meta.env.VITE_SERVER_URL,
-    localStorageAccessTokenKey: import.meta.env.VITE_LOCAL_STORAGE_ACCESS_TOKEN_KEY,
-    localStorageRefreshTokenKey: import.meta.env.VITE_LOCAL_STORAGE_REFRESH_TOKEN_KEY,
+    serverUrl: "https://us-central1-phare-app-6fd56.cloudfunctions.net/app",
+    localStorageAccessTokenKey: "phareAccessToken",
+    localStorageRefreshTokenKey: "phareRefreshToken",
 
-    defaultCounsellingRate: import.meta.env.VITE_DEFAULT_COUNSELLING_RATE,
-    defaultSupervisingRate: import.meta.env.VITE_DEFAULT_SUPERVISING_RATE,
+    defaultCounsellingRate: 150,
+    defaultSupervisingRate: 150,
 }
 
 export default config;
